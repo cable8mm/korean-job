9d03d9c90d8d039512197918894bad5a04b20649
Sam Lee <cable8mm@gmail.com>
2024-01-31 12:10:31 +1300

    Uninstall laravel/folio

--- config/app.php
+++ config/app.php
@@ -170,7 +170,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FolioServiceProvider::class,
     ])->toArray(),
 
     /*
