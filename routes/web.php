a6cc908f12c928c40155a0ed1ca7c6a400e22d80
Samgu Lee <cable8mm@gmail.com>
2024-01-31 01:33:19 +1300

    I8n (#17)
    * Install mcamara/laravel-localization package
    
    * Configure mcamara/laravel-localization and add switcher

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QnaController;
 use App\Http\Controllers\Social\AuthenticatedSocialController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,42 +22,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', HomeController::class)->name('home');
+Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+    Route::get('/', HomeController::class)->name('home');
+    Route::get('/job', [JobController::class, 'index'])->name('job');
+    Route::get('/job/{job}', [JobController::class, 'show'])->name('job.show')->whereNumber('job');
+    Route::middleware('auth')->get('/job/create', [JobController::class, 'create'])->name('job.create');
+    Route::middleware('auth')->post('/job', [JobController::class, 'store'])->name('job.store');
+    Route::middleware('auth')->get('/job/{job}/edit', [JobController::class, 'edit'])->name('job.edit')->whereNumber('job');
+    Route::middleware('auth')->patch('/job/{job}', [JobController::class, 'update'])->name('job.update')->whereNumber('job');
+    Route::middleware('auth')->delete('/job/{job}', [JobController::class, 'destroy'])->name('job.destroy')->whereNumber('job');
 
-Route::get('/job', [JobController::class, 'index'])->name('job');
-Route::get('/job/{job}', [JobController::class, 'show'])->name('job.show')->whereNumber('job');
-Route::middleware('auth')->get('/job/create', [JobController::class, 'create'])->name('job.create');
-Route::middleware('auth')->post('/job', [JobController::class, 'store'])->name('job.store');
-Route::middleware('auth')->get('/job/{job}/edit', [JobController::class, 'edit'])->name('job.edit')->whereNumber('job');
-Route::middleware('auth')->patch('/job/{job}', [JobController::class, 'update'])->name('job.update')->whereNumber('job');
-Route::middleware('auth')->delete('/job/{job}', [JobController::class, 'destroy'])->name('job.destroy')->whereNumber('job');
+    Route::get('/notice', [NoticeController::class, 'index'])->name('notice');
+    Route::get('/notice/{notice}', [NoticeController::class, 'show'])->name('notice.show');
 
-Route::get('/notice', [NoticeController::class, 'index'])->name('notice');
-Route::get('/notice/{notice}', [NoticeController::class, 'show'])->name('notice.show');
+    Route::get('/qna', [QnaController::class, 'index'])->name('qna');
+    Route::get('/qna/{qna}', [QnaController::class, 'show'])->name('qna.show')->whereNumber('qna');
+    Route::middleware('auth')->get('/qna/create', [QnaController::class, 'create'])->name('qna.create');
+    Route::middleware('auth')->post('/qna', [QnaController::class, 'store'])->name('qna.store');
+    Route::middleware('auth')->get('/qna/{qna}/edit', [QnaController::class, 'edit'])->name('qna.edit')->whereNumber('qna');
+    Route::middleware('auth')->patch('/qna/{qna}', [QnaController::class, 'update'])->name('qna.update')->whereNumber('qna');
+    Route::middleware('auth')->delete('/qna/{qna}', [QnaController::class, 'destroy'])->name('qna.destroy')->whereNumber('qna');
 
-Route::get('/qna', [QnaController::class, 'index'])->name('qna');
-Route::get('/qna/{qna}', [QnaController::class, 'show'])->name('qna.show')->whereNumber('qna');
-Route::middleware('auth')->get('/qna/create', [QnaController::class, 'create'])->name('qna.create');
-Route::middleware('auth')->post('/qna', [QnaController::class, 'store'])->name('qna.store');
-Route::middleware('auth')->get('/qna/{qna}/edit', [QnaController::class, 'edit'])->name('qna.edit')->whereNumber('qna');
-Route::middleware('auth')->patch('/qna/{qna}', [QnaController::class, 'update'])->name('qna.update')->whereNumber('qna');
-Route::middleware('auth')->delete('/qna/{qna}', [QnaController::class, 'destroy'])->name('qna.destroy')->whereNumber('qna');
+    Route::get('/community', [CommunityController::class, 'index'])->name('community');
 
-Route::get('/community', [CommunityController::class, 'index'])->name('community');
+    Route::get('/post', [PostController::class, 'index'])->name('post');
+    Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show')->whereNumber('post');
+    Route::middleware('auth')->get('/post/create', [PostController::class, 'create'])->name('post.create');
+    Route::middleware('auth')->post('/post', [PostController::class, 'store'])->name('post.store');
+    Route::middleware('auth')->get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit')->whereNumber('post');
+    Route::middleware('auth')->patch('/post/{post}', [PostController::class, 'update'])->name('post.update')->whereNumber('post');
+    Route::middleware('auth')->delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy')->whereNumber('post');
 
-Route::get('/post', [PostController::class, 'index'])->name('post');
-Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show')->whereNumber('post');
-Route::middleware('auth')->get('/post/create', [PostController::class, 'create'])->name('post.create');
-Route::middleware('auth')->post('/post', [PostController::class, 'store'])->name('post.store');
-Route::middleware('auth')->get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit')->whereNumber('post');
-Route::middleware('auth')->patch('/post/{post}', [PostController::class, 'update'])->name('post.update')->whereNumber('post');
-Route::middleware('auth')->delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy')->whereNumber('post');
+    Route::middleware('guest')->get('/social/{provider}/redirect', [AuthenticatedSocialController::class, 'redirectProvider'])->name('social.redirect');
+    Route::middleware('guest')->get('/social/{provider}/callback', [AuthenticatedSocialController::class, 'callbackProvider'])->name('social.callback');
 
-Route::middleware('guest')->get('/social/{provider}/redirect', [AuthenticatedSocialController::class, 'redirectProvider'])->name('social.redirect');
-Route::middleware('guest')->get('/social/{provider}/callback', [AuthenticatedSocialController::class, 'callbackProvider'])->name('social.callback');
-
-Route::middleware('auth')->get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::middleware('auth')->patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::middleware('auth')->delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware('auth')->get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::middleware('auth')->patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::middleware('auth')->delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
